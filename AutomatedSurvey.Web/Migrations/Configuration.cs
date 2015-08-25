1f8fd21569151743fe1bffe6df57be780d52af1c
Agustin Camino <agustin.camino@gmail.com>
2015-08-25 14:04:43 -0500

    update Seed method to create surveys and questions

--- AutomatedSurvey.Web/Migrations/Configuration.cs
+++ AutomatedSurvey.Web/Migrations/Configuration.cs
@@ -12,18 +12,40 @@ namespace AutomatedSurvey.Web.Migrations
 
         protected override void Seed(AutomatedSurveysContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.Surveys.AddOrUpdate(
+                survey => new { survey.Id, survey.Title },
+                new Survey { Id = 1, Title = "Twilio" });
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.SaveChanges();
+
+            context.Questions.AddOrUpdate(
+                question => new { question.Body, question.Type, question.SurveyId },
+                new Question
+                {
+                    Body = "Hello. Thanks for taking the Twilio Developer Education survey. On a scale of 0 to 9 how would you rate this tutorial?",
+                    Type = QuestionType.Numeric,
+                    SurveyId = 1
+                },
+                new Question
+                {
+                    Body = "On a scale of 0 to 9 how would you rate the design of this tutorial?",
+                    Type = QuestionType.Numeric,
+                    SurveyId = 1
+                },
+                new Question
+                {
+                    Body = "In your own words please describe your feelings about Twilio right now? Press the pound sign when you are finished.",
+                    Type = QuestionType.Voice,
+                    SurveyId = 1
+                },
+                new Question
+                {
+                    Body = "Do you like my voice? Please be honest, I dislike liars.",
+                    Type = QuestionType.YesNo,
+                    SurveyId = 1
+                });
+
+            context.SaveChanges();
         }
     }
 }
