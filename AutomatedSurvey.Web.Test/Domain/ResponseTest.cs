45aa7722e490545a8d7f1f1de9fb560eca956309
Agustin Camino <agustin.camino@gmail.com>
2015-08-25 14:23:13 -0500

    add response domain object
    This object is the reposible to encapsulate the logic to generate a
    `TwilioResponse`.

---
+++ AutomatedSurvey.Web.Test/Domain/ResponseTest.cs
@@ -0,0 +1,46 @@
+﻿using AutomatedSurvey.Web.Domain;
+using AutomatedSurvey.Web.Models;
+using NUnit.Framework;
+
+namespace AutomatedSurvey.Web.Test.Domain
+{
+    public class ResponseTest
+    {
+        [Test]
+        public void Build_a_response_using_Record_if_the_question_type_is_Voice()
+        {
+            var question = new Question
+            {
+                Id = 1,
+                Body = "How's the weather?",
+                Type = QuestionType.Voice
+            };
+            var response = new Response(question).Build();
+            var expectedResponse = string.Format(
+                "<Response>\r\n" +
+                "  <Say>{0}</Say>\r\n" +
+                "  <Say>{1}</Say>\r\n" +
+                "  <Record action=\"/answers/create?questionId={2}\" />\r\n" +
+                "</Response>", question.Body, Response.QuestionTypeToMessage[question.Type], question.Id);
+
+            Assert.That(response.ToString(), Is.EqualTo(expectedResponse));
+        }
+
+        [TestCase(1, "On a scale of 0 to 9 how much do you like ice cream?", QuestionType.Numeric)]
+        [TestCase(2, "Do you like my classical voice? 1 for yes and 0 for no", QuestionType.YesNo)]
+        public void Build_a_response_using_Gather_if_the_question_type_is_Numeric_or_YesNo(
+            int id, string body, QuestionType type)
+        {
+            var question = new Question { Id = id, Body = body, Type = type };
+            var response = new Response(question).Build();
+            var expectedResponse = string.Format(
+                "<Response>\r\n" +
+                "  <Say>{0}</Say>\r\n" +
+                "  <Say>{1}</Say>\r\n" +
+                "  <Gather action=\"/answers/create?questionId={2}\" />\r\n" +
+                "</Response>", question.Body, Response.QuestionTypeToMessage[question.Type], question.Id);
+
+            Assert.That(response.ToString(), Is.EqualTo(expectedResponse));
+        }
+    }
+}
---
+++ AutomatedSurvey.Web/Domain/Response.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using AutomatedSurvey.Web.Models;
+using Twilio.TwiML;
+
+namespace AutomatedSurvey.Web.Domain
+{
+    public class Response
+    {
+        private readonly Question _question;
+
+        public Response(Question question)
+        {
+            _question = question;
+        }
+
+        public static IDictionary<QuestionType, string> QuestionTypeToMessage
+        {
+            get
+            {
+                return new Dictionary<QuestionType, string>
+                {
+                    {QuestionType.Voice, "Please record your answer after the beep and then hit the pound sign"},
+                    {QuestionType.Numeric, "Please press a number between 0 and 9 and then hit the pound sign"},
+                    {QuestionType.YesNo, "Please press the 1 for yes and the 0 for no and then hit the pound sign"}
+                };
+            }
+        }
+
+        /// <summary>
+        /// Builds an instance.
+        /// </summary>
+        /// <returns>A new instance of the TwilioResponse</returns>
+        public TwilioResponse Build()
+        {
+            var response = new TwilioResponse();
+            response.Say(_question.Body);
+            response.Say(QuestionTypeToMessage[_question.Type]);
+            AddRecordOrGatherCommands(response);
+
+            return response;
+        }
+
+        private void AddRecordOrGatherCommands(TwilioResponse response)
+        {
+            var questionType = _question.Type;
+            switch (questionType)
+            {
+                case QuestionType.Voice:
+                    response.Record(new { action = GenerateUrl(_question) });
+                    break;
+                case QuestionType.Numeric:
+                case QuestionType.YesNo:
+                    response.Gather(new { action = GenerateUrl(_question) });
+                    break;
+            }
+        }
+
+        private static string GenerateUrl(Question question)
+        {
+            return string.Format("/answers/create?questionId={0}", question.Id);
+        }
+    }
+}
