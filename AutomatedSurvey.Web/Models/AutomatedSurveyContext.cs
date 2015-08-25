f37a1af58ad48ea2dc78ddcbe24e62758aca10ab
Agustin Camino <agustin.camino@gmail.com>
2015-08-25 13:57:37 -0500

    enable migrations

---
+++ AutomatedSurvey.Web/Models/AutomatedSurveyContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace AutomatedSurvey.Web.Models
+{
+    public class AutomatedSurveysContext : DbContext
+    {
+        public AutomatedSurveysContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Survey> Surveys { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+    }
+}
