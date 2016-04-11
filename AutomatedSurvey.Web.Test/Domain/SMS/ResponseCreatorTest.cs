441990008d0735c1224ec8e3e9bf647cc43158d4
Agustin Camino <agustin.camino@gmail.com>
2016-04-11 15:40:56 -0500

    Add SMS.ResponseCreator

---
+++ AutomatedSurvey.Web.Test/Domain/SMS/ResponseCreatorTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Xml.XPath;
+using AutomatedSurvey.Web.Domain.SMS;
+using AutomatedSurvey.Web.Models;
+using NUnit.Framework;
+
+#pragma warning disable 219 // Disable null assigment;
+
+namespace AutomatedSurvey.Web.Test.Domain.SMS
+{
+    public class ResponseCreatorTest
+    {
+        [Test]
+        public void WhenTheQuestionIsNull_ThenItCreatesAClosingMessage()
+        {
+            Question question = null;
+
+            var responseCreator = new ResponseCreator(null);
+            var response = responseCreator.Create();
+
+            var data = response.ToXDocument();
+            Assert.That(
+                data.XPathSelectElement("Response/Message").Value,
+                Is.EqualTo("Thanks for your time. Good bye"));
+        }
+
+        [TestCase(QuestionType.Voice, "Free Question")]
+        [TestCase(QuestionType.Numeric, "Numeric Question")]
+        [TestCase(QuestionType.YesNo, "Yes No Question")]
+        public void WhenTheQuestionIsNotNull_ThenItCreatesAResponseWithTheAppropriateQuestion(
+            QuestionType questionType, string questionBody  )
+        {
+            var question = new Question {Type = questionType, Body = questionBody};
+
+            var responseCreator = new ResponseCreator(question);
+            var response = responseCreator.Create();
+
+            var data = response.ToXDocument();
+            StringAssert.Contains(
+                questionBody, data.XPathSelectElement("Response/Message").Value);
+        }
+
+        [TestCase(QuestionType.Voice, "Reply to this message with your answer")]
+        [TestCase(QuestionType.Numeric, "Reply with a number from \"0\" to \"9\" to this message")]
+        [TestCase(QuestionType.YesNo, "Reply with \"1\" for YES and \"0\" for NO to this message")]
+        public void WhenTheQuestionIsNotNull_ThenItCreatesAResponseWithTheAppropriateInstruction(
+            QuestionType questionType, string expectedInstructions)
+        {
+            var question = new Question {Type = questionType};
+
+            var responseCreator = new ResponseCreator(question);
+            var response = responseCreator.Create();
+
+            var data = response.ToXDocument();
+            StringAssert.Contains(
+                expectedInstructions, data.XPathSelectElement("Response/Message").Value);
+        }
+    }
+}
---
+++ AutomatedSurvey.Web/Domain/SMS/ResponseCreator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using AutomatedSurvey.Web.Models;
+using Twilio.TwiML;
+
+namespace AutomatedSurvey.Web.Domain.SMS
+{
+    public interface IResponseCreator
+    {
+        TwilioResponse Create();
+    }
+
+    public class ResponseCreator : IResponseCreator
+    {
+        private readonly Question _question;
+
+        public ResponseCreator(Question question)
+        {
+            _question = question;
+        }
+
+        public static IDictionary<QuestionType, string> Instructions
+        {
+            get
+            {
+                return new Dictionary<QuestionType, string>
+                {
+                    {QuestionType.Voice, "Reply to this message with your answer"},
+                    {QuestionType.Numeric, "Reply with a number from \"0\" to \"9\" to this message"},
+                    {QuestionType.YesNo, "Reply with \"1\" for YES and \"0\" for NO to this message"}
+                };
+            }
+        }
+
+        public TwilioResponse Create()
+        {
+            if (_question == null)
+            {
+                return ExitMessage();
+            }
+
+            var response = new TwilioResponse();
+            response.Message(MessageBody());
+
+            return response;
+        }
+
+        private string MessageBody()
+        {
+            return string.Format("{0}\n\n{1}", _question.Body, Instructions[_question.Type]);
+        }
+
+        private static TwilioResponse ExitMessage()
+        {
+            var response = new TwilioResponse();
+            response.Message("Thanks for your time. Good bye");
+
+            return response;
+        }
+    }
+}
